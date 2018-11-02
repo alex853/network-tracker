42efa484b6712fd6f98080d5e0dc8bcb684d3f6c
alexey853 <alexxxey.kornev@gmail.com>
2018-11-02 20:16:50 +0400

    Flights db script and logging configuration

--- flights/conf/db/flights-database.sql
+++ flights/conf/db/flights-database.sql
@@ -1,50 +1,77 @@
 
-CREATE SEQUENCE flight_id_seq;
+CREATE SEQUENCE flt_pilot_status_id_seq;
 
-CREATE TABLE flight (
-    id integer DEFAULT nextval('flight_id_seq'::regclass) NOT NULL CONSTRAINT pk_flight PRIMARY KEY,
+CREATE TABLE flt_pilot_status (
+    id integer DEFAULT nextval('flt_pilot_status_id_seq'::regclass) NOT NULL CONSTRAINT pk_pilot_status PRIMARY KEY,
     version smallint NOT NULL,
 
-    create_dt timestamp without time zone NOT NULL,
-    modify_dt timestamp without time zone NOT NULL,
+    create_dt timestamp NOT NULL,
+    modify_dt timestamp NOT NULL,
 
-    network smallint NOT NULL,
+    pilot_number integer NOT NULL,
+
+    last_seen_report_id integer NOT NULL,
+    last_seen_dt timestamp NOT NULL,
+
+    last_processed_report_id integer NOT NULL,
+    last_processed_dt timestamp NOT NULL,
+
+    curr_flight_id integer
+);
+
+CREATE SEQUENCE flt_flight_id_seq;
+
+CREATE TABLE flt_flight (
+    id integer DEFAULT nextval('flt_flight_id_seq'::regclass) NOT NULL CONSTRAINT pk_flt_flight PRIMARY KEY,
+    version smallint NOT NULL,
+
+    create_dt timestamp NOT NULL,
+    modify_dt timestamp NOT NULL,
+
+    --network smallint NOT NULL,
     pilot_number integer NOT NULL,
 
     callsign character varying(10) NOT NULL,
     aircraft_type character varying(10),
     reg_no character varying(10),
-    planned_origin character varying(10),
+    planned_departure character varying(10),
     planned_destination character varying(10),
 
     status smallint NOT NULL,
 
-    first_seen_report_id integer NOT NUL,
-    first_seen_dt timestamp without time zone NOT NULL,
+    first_seen_report_id integer NOT NULL,
+    first_seen_dt timestamp NOT NULL,
 
-    last_seen_report_id integer NOT NUL,
-    last_seen_dt timestamp without time zone NOT NULL,
+    last_seen_report_id integer NOT NULL,
+    last_seen_dt timestamp NOT NULL,
 
     departure_report_id integer,
-    departure_dt timestamp without time zone,
+    departure_dt timestamp,
     departure_latitude real,
     departure_longitude real,
-    origin_type smallint,
-    origin_icao character varying(10),
+    departure_type smallint,
+    departure_icao character varying(10),
 
-    arrival_report_id integer NOT NUL,
-    arrival_dt timestamp without time zone,
+    arrival_report_id integer,
+    arrival_dt timestamp,
     arrival_latitude real,
     arrival_longitude real,
-    origin_type smallint,
-    origin_icao character varying(10),
+    arrival_type smallint,
+    arrival_icao character varying(10),
 
     distance_flown real,
     flight_time real
 );
 
-CREATE INDEX flight_modify_dt on flight (modify_dt);
-CREATE INDEX flight_pilot_number_first_seen on flight (pilot_number, first_seen_report_id);
+--CREATE INDEX flt_flight_modify_dt on flight (modify_dt);
+--CREATE INDEX flight_pilot_number_first_seen on flight (pilot_number, first_seen_report_id);
 
-ALTER TABLE ONLY flight
-    ADD CONSTRAINT uk_flight_pilot_number_first_seen_network UNIQUE (pilot_number, first_seen_report_id, network);
+ALTER TABLE ONLY flt_pilot_status
+    ADD CONSTRAINT uk_flt_pilot_status_pilot_number UNIQUE (pilot_number);
+
+ALTER TABLE ONLY flt_flight
+    ADD CONSTRAINT uk_flt_flight_pilot_number_first_seen_network UNIQUE (pilot_number, first_seen_report_id);
+
+ALTER TABLE ONLY flt_pilot_status
+    ADD CONSTRAINT fk_flt_pilot_status_curr_flight_id FOREIGN KEY (curr_flight_id) REFERENCES flt_flight(id);
+
