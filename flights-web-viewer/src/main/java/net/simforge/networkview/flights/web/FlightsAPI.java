899e6aa4be7ead10487b0308d3d6bdf7d29ffc3b
alexey853 <alexxxey.kornev@gmail.com>
2018-11-11 19:53:08 +0400

    Flights-Web-Viewer - Pilot flights page

--- flights-web-viewer/src/main/java/net/simforge/networkview/flights/web/FlightsAPI.java
+++ flights-web-viewer/src/main/java/net/simforge/networkview/flights/web/FlightsAPI.java
@@ -12,9 +12,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -50,7 +48,7 @@ public class FlightsAPI {
 //        this.auth = new AuthHelper(request);
     }
 
-    @POST
+    @GET
     @Path("active-flights")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getActiveFlights() {
@@ -64,30 +62,7 @@ public class FlightsAPI {
                     .setParameter("lastSeenDt", JavaTime.nowUtc().minusHours(1))
                     .list();
 
-            List<Map<String, String>> flightDtos = new ArrayList<>();
-            for (DBFlight flight : flights) {
-                Map<String, String> flightDto = new HashMap<>();
-                flightDto.put("callsign", flight.getCallsign());
-                flightDto.put("pilotNumber", String.valueOf(flight.getPilotNumber()));
-                flightDto.put("aircraftType", flight.getAircraftType());
-                flightDto.put("regNo", flight.getRegNo());
-                flightDto.put("fpRoute", printIcao(flight.getPlannedDeparture()) + " - " + printIcao(flight.getPlannedDestination()));
-                flightDto.put("status", FlightStatus.byCode(flight.getStatus()).name());
-                flightDto.put("takeoff", flight.getTakeoffIcao() != null || flight.getTakeoffDt() != null
-                            ? printIcao(flight.getTakeoffIcao()) + " at " + printTime(flight.getTakeoffDt())
-                            : "");
-                flightDto.put("landing", flight.getLandingIcao() != null || flight.getLandingDt() != null
-                            ? printIcao(flight.getLandingIcao()) + " at " + printTime(flight.getLandingDt())
-                            : "");
-                flightDto.put("distance", flight.getDistanceFlown() != null
-                            ? String.valueOf(flight.getDistanceFlown().intValue()) + " nm"
-                            : "");
-                flightDto.put("time", flight.getFlightTime() != null
-                            ? JavaTime.toHhmm(Duration.of((long) (flight.getFlightTime() * 60), ChronoUnit.MINUTES))
-                            : "");
-
-                flightDtos.add(flightDto);
-            }
+            List<Map<String, Object>> flightDtos = dbFlightsToMaps(flights);
 
             return Response.ok(RestUtils.success(flightDtos)).build();
         } catch (Exception e) {
@@ -100,10 +75,92 @@ public class FlightsAPI {
         }
     }
 
-    private static final DateTimeFormatter hhmmdM = DateTimeFormatter.ofPattern("HH:mm dd-MM");
+    @GET
+    @Path("pilot-flights")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getPilotFlights(@QueryParam("pilotNumber") String pilotNumber) {
+        BM.start("FlightsAPI.getPilotFlights");
+        try (Session session = webAppContext.openSession()) {
+
+            //noinspection JpaQlInspection,unchecked
+            List<DBFlight> flights = session
+                    .createQuery("from Flight where pilotNumber = :pilotNumber order by firstSeenDt desc")
+                    .setInteger("pilotNumber", Integer.parseInt(pilotNumber))
+                    .list();
+
+            List<Map<String, Object>> flightDtos = dbFlightsToMaps(flights);
+
+            return Response.ok(RestUtils.success(flightDtos)).build();
+        } catch (Exception e) {
+            logger.error("Could not load pilot flights", e);
+
+            String msg = String.format("Could not load pilot flights: %s", Misc.messagesBr(e));
+            return Response.ok(RestUtils.failure(msg)).build();
+        } finally {
+            BM.stop();
+        }
+    }
+
+    private List<Map<String, Object>> dbFlightsToMaps(List<DBFlight> flights) {
+        List<Map<String, Object>> flightDtos = new ArrayList<>();
+        for (DBFlight flight : flights) {
+            Map<String, Object> flightDto = new HashMap<>();
+
+            flightDto.put("callsign", flight.getCallsign());
+            flightDto.put("pilotNumber", String.valueOf(flight.getPilotNumber()));
+            flightDto.put("aircraftType", flight.getAircraftType());
+            flightDto.put("regNo", flight.getRegNo());
+
+            flightDto.put("status", FlightStatus.byCode(flight.getStatus()).name()); // todo remove it
+            flightDto.put("statusText", FlightStatus.byCode(flight.getStatus()).name());
+            flightDto.put("statusCode", String.valueOf(flight.getStatus()));
+
+            flightDto.put("firstSeenDt", printTime(flight.getFirstSeenDt()));
+
+            flightDto.put("lastSeenDt", printTime(flight.getLastSeenDt()));
+
+            flightDto.put("takeoffIcao", flight.getTakeoffIcao());
+            flightDto.put("takeoffDt", printTime(flight.getTakeoffDt()));
+            flightDto.put("takeoffLat", flight.getTakeoffLatitude());
+            flightDto.put("takeoffLng", flight.getTakeoffLongitude());
+
+            flightDto.put("landingIcao", flight.getLandingIcao());
+            flightDto.put("landingDt", printTime(flight.getLandingDt()));
+            flightDto.put("landingLat", flight.getLandingLatitude());
+            flightDto.put("landingLng", flight.getLandingLongitude());
+
+            flightDto.put("distance", flight.getDistanceFlown() != null
+                    ? String.valueOf(flight.getDistanceFlown().intValue()) + " nm"
+                    : "");
+            flightDto.put("time", flight.getFlightTime() != null
+                    ? JavaTime.toHhmm(Duration.of((long) (flight.getFlightTime() * 60), ChronoUnit.MINUTES))
+                    : "");
+
+
+
+
+
+            flightDto.put("fpRoute", printIcao(flight.getPlannedDeparture()) + " - " + printIcao(flight.getPlannedDestination()));
+            flightDto.put("takeoff", flight.getTakeoffIcao() != null || flight.getTakeoffDt() != null
+                    ? printIcao(flight.getTakeoffIcao()) + " at " + printTime(flight.getTakeoffDt())
+                    : "");
+            flightDto.put("landing", flight.getLandingIcao() != null || flight.getLandingDt() != null
+                    ? printIcao(flight.getLandingIcao()) + " at " + printTime(flight.getLandingDt())
+                    : "");
+
+            flightDtos.add(flightDto);
+        }
+        return flightDtos;
+    }
+
+    private static final DateTimeFormatter hhmmdM = DateTimeFormatter.ofPattern("HH:mm dd/MM");
     private static final DateTimeFormatter hhmm = DateTimeFormatter.ofPattern("HH:mm");
 
     private String printTime(LocalDateTime dt) {
+        if (dt == null) {
+            return null;
+        }
+
         LocalDate today = JavaTime.todayUtc();
         LocalDate dtDay = dt.toLocalDate();
         if (today.equals(dtDay)) {
