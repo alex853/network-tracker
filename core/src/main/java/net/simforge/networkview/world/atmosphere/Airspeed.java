1d3d91236aacfd46dfffaa71884302ef7cdd992f
alex853 <alexxxey.kornev@gmail.com>
2020-06-27 17:21:04 +0400

    Airspeed conversions

---
+++ core/src/main/java/net/simforge/networkview/world/atmosphere/Airspeed.java
@@ -0,0 +1,11 @@
+package net.simforge.networkview.world.atmosphere;
+
+public class Airspeed {
+    public static int iasToTas(int ias, int altitude) {
+        return (int) (ias * .02 * altitude / 1000) + ias;
+    }
+
+    public static int tasToIas(int tas, int altitude) {
+        return (int) (tas / (.02 * altitude / 1000 + 1));
+    }
+}
